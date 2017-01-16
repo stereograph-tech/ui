6f5e2c82fa9694f52ea6fa865901dab3201c0b80
theBull <daniel.p.bullis@gmail.com>
2017-01-15 19:14:08 -0800

    Version update

--- ui/index.ts
+++ ui/index.ts
@@ -1,27 +1,28 @@
+export module UI {
+	interface Config {
+		grid: GridConfig;
+	}
 
-export interface Config {
-	grid: GridConfig;
-}
+	interface GridConfig {
+		base: number;
+		unit: string;
+		filename: string;	
+		classes: ClassConfig[];			
+	}
 
-export interface GridConfig {
-	base: number;
-	unit: string;
-	filename: string;	
-	classes: ClassConfig[];			
-}
+	interface ClassConfig {
+		name: string;
+		decimals?: boolean;
+		description: string;
+		min: number;
+		max: number;
+		increment: number;
+		properties: ClassPropertiesConfig[];
+	}
 
-export interface ClassConfig {
-	name: string;
-	decimals?: boolean;
-	description: string;
-	min: number;
-	max: number;
-	increment: number;
-	properties: ClassPropertiesConfig[];
-}
-
-export interface ClassPropertiesConfig {
-	name: string;
-	value: string|number;
-	important: boolean;
+	interface ClassPropertiesConfig {
+		name: string;
+		value: string|number;
+		important: boolean;
+	}
 }
