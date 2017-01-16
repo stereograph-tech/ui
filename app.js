05e87055dba93e5647a6fb1c20d16b076f0e6820
theBull <daniel.p.bullis@gmail.com>
2017-01-15 23:37:38 -0800

    Version update

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require('./cors');
+
 var app = express();
- 
+
+app.use(cors());
+app.all('/*', cors());
+var routes = require("./routes/routes.js")(app);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'))
-
-var routes = require("./routes/routes.js")(app);
+app.use(express.static('public'));
  
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+   console.log("Listening on port %s...", server.address().port);
 });
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
 });
